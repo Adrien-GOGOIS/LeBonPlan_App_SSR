bbfa56c6f300b473ef61449fd98bec25836e69b1
Adrien GOGOIS <ad.gogois@gmail.com>
2022-04-05 12:01:59 +0200

    Add profile/login/signup pages and redirection when form submit

--- index.js
+++ index.js
@@ -16,12 +16,32 @@ app.get("/", (req, res) => {
   res.render("homepage");
 });
 
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
+
 app.get("/users", (req, res) => {
   res.render("users");
 });
 
 app.get("/products", (req, res) => {
   res.render("products");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/login", (req, res) => {
+  res.redirect("/profile");
+});
+
+app.post("/signup", (req, res) => {
+  res.redirect("/");
 });
 
 app.listen(8000, () => console.log("Listen on port 8000"));
